40b821fa3bc819349d08860e707c31ab83b960c5
MomchilDeus <momchil.kolev@protonmail.com>
2018-07-15 13:15:08 +0300

    My solution to the Group exercise in Eloquent JavaScript Ch.6

---
+++ Group.js
@@ -0,0 +1,14 @@
+// A simple class (or 'data type' whatever you will), from Eloquent JavaScript Ch. 6
+class Group {
+  constructor(group = group) {
+    this.group = group
+  }
+  has(val) { return this.group.includes(val) }
+  add(val) { if (!this.group.includes(val)) this.group.push(val) }
+  delete(val) { if (this.group.includes(val)) this.group.splice(this.group.indexOf(val), 1) }
+  static from(iterable) {
+    let group = []
+    for (let item of iterable) { group.push(item) }
+    return new Group(group)
+  }
+}
